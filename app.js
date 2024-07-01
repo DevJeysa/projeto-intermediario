e3d229fcb2920fe06262620077c700d09846a149
Jeysa <0000663302@senaimgaluno.com.br>
2024-07-01 20:40:54 -0300

    Adiciona nova funcionalidade ao app.js

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
 "console.log('Vers∆o 1.0');" 
+<<<<<<< HEAD
 "console.log('Mudanáa na master');" 
+=======
+"console.log('Nova funcionalidade');" 
+>>>>>>> acac3d5 (Adiciona nova funcionalidade ao app.js)
